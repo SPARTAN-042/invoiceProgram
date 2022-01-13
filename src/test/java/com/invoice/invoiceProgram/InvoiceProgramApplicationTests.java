535cf3ef9e1bd01cdb77df882e96bdff5862762c
SPARTAN-042 <sentinal042@gmail.com>
2022-01-13 17:49:40 +0000

    Initial commit

---
+++ src/test/java/com/invoice/invoiceProgram/InvoiceProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invoice.invoiceProgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
